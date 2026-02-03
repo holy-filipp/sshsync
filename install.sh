7550b3ffa85167174370c80e3a0e4f906279a668
Filipp <im@filipp.su>
2026-02-03 20:20:05 +0400

    added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+RELEASES_URL="https://api.github.com/repos/holy-filipp/sshsync/releases"
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+  DOWNLOAD_PATH=$(curl $RELEASES_URL | jq -r '.[0].assets.[] | select(.name | endswith("linux-amd64.tar.gz")) | .browser_download_url')
+elif [ "$UNAME" == "Darwin" ] ; then
+	DOWNLOAD_PATH=$(curl $RELEASES_URL | jq -r '.[0].assets.[] | select(.name | endswith("darwin-arm64.tar.gz")) | .browser_download_url')
+fi
+
+wget -qO- "$DOWNLOAD_PATH" | gunzip | tar xvf -
